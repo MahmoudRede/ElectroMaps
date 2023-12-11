3642666aaadb97e553a9cc598d84ccc95c09313e
Maher Adel <74119717+maherade@users.noreply.github.com>
2023-12-11 12:07:27 +0200

    elevated button station

--- lib/presentation/screens/stations_screen/stations_screen.dart
+++ lib/presentation/screens/stations_screen/stations_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_electromaps/styles/colors/color_manager.dart';
 import 'package:flutter/material.dart';
 
 class StationsScreen extends StatelessWidget {
@@ -5,8 +6,21 @@ class StationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Center(
-      child: Text('Stations Screen'),
+    return  SafeArea(
+      child: Scaffold(
+      floatingActionButton:  FloatingActionButton(
+        shape: const CircleBorder(),
+        backgroundColor: ColorManager.primaryColor,
+        onPressed: () {},
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.add_rounded,color: ColorManager.white,size: MediaQuery.sizeOf(context).width * 0.05),
+            Icon(Icons.ev_station,color: ColorManager.white,size: MediaQuery.sizeOf(context).width * 0.072),
+          ]
+        ),
+       ),
+      ),
     );
   }
 }
